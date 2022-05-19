c766c6a7270bbb52981da55c974cad1cd8de40f9
Vincentkovsky <1041866971@qq.com>
2022-05-19 21:55:53 +1000

    aaa

--- constants.js
+++ constants.js
@@ -1,2 +1,15 @@
-const carts = [];
-const name = '';
+const carts = [
+  {
+    Category: 'Sedan',
+    Availability: true,
+    Brand: 'Toyota',
+    Model: 'Camry',
+    Modelyear: 2013,
+    Mileage: 10000,
+    Fueltype: 'Petrol',
+    Seats: 5,
+    Priceperday: '$120',
+    Description: 'xxxxxxx',
+  },
+];
+const name = { firstName: 'a', lastName: 'b' };
